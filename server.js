f4db5e13ec5524e7b3faf4dfe8546c3e6d5a4ecf
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-02 10:01:08 +0900

    1. 초기설정

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT)
+
+db.on('error', (err) => {
+    console.log(err);
+});
+
+db.once('open', () => {
+    console.log('디비 연결');
+});
